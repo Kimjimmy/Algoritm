a27cca49f3de9756c1f62a92f457b222667c19d3
Kimjimmy <wldlf94@naver.com>
2021-10-17 20:56:39 +0900

    DP_KnapSack

---
+++ Algorithm/DP_KnapSack.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using std::cin; using std::cout;
+using std::vector;
+
+int n, k;
+int weight[101];
+int value[101];
+int opt[101][100001];
+
+int dp(int n, int k) {
+
+	for (int i = 0; i <= n; i++) {
+		opt[i][0] = 0;
+	}
+	for (int j = 0; j <= k; j++) {
+		opt[0][j] = 0;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= k; j++) {
+			if (weight[i] > j) {
+				opt[i][j] = opt[i - 1][j];
+				cout << opt[i][j] << " ";
+				continue;
+			}
+			if (opt[i - 1][j] > opt[i - 1][j - weight[i]] + value[i]) {
+				opt[i][j] = opt[i - 1][j];
+				cout << opt[i][j] << " ";
+			}
+			else {
+				opt[i][j] = opt[i - 1][j - weight[i]] + value[i];
+				cout << opt[i][j] << " ";
+			}
+		}
+		cout << '\n';
+	}
+	return opt[n][k];
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> weight[i] >> value[i];
+	}
+	dp(n, k);
+
+
+
+
+	return 0;
+}
