160235e021a9e0f5d6acea726f06e62efc4e5295
Kimjimmy <wldlf94@naver.com>
2021-10-17 21:02:03 +0900

    DP_KnapSack

--- BOJ/DP_KnapSack_12865.cpp
+++ BOJ/DP_KnapSack_12865.cpp
@@ -10,41 +10,38 @@ int weight[101];
 int value[101];
 int opt[101][100001];
 
-int dp(int n,int k) {
-	
-	// 0 행에 0 설정.
-	// 0 열에 0 설정.
-	/*for (int i = 0; i <= n; i++) {
-		for (int j = 0; j <= k; j++) {
-			opt[i].push_back(0);
-		}
-	}*/
+void dp(int n, int k) {
+
+	// 0 행에 0 으로 초기화.
 	for (int i = 0; i <= n; i++) {
 		opt[i][0] = 0;
 	}
+
+	// 0 열에 0 으로 초기화.
 	for (int j = 0; j <= k; j++) {
 		opt[0][j] = 0;
 	}
-	
+	// 순환식 풀이
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= k; j++) {
 			if (weight[i] > j) {
 				opt[i][j] = opt[i - 1][j];
-				cout << opt[i][j] << " ";
+				//cout << opt[i][j] << " ";
 				continue;
 			}
 			if (opt[i - 1][j] > opt[i - 1][j - weight[i]] + value[i]) {
 				opt[i][j] = opt[i - 1][j];
-				cout << opt[i][j] << " ";
+				//cout << opt[i][j] << " ";
 			}
 			else {
 				opt[i][j] = opt[i - 1][j - weight[i]] + value[i];
-				cout << opt[i][j] << " ";
+				//cout << opt[i][j] << " ";
 			}
 		}
-		cout << '\n';
+		//cout << '\n';
 	}
-	return opt[n][k];
+	// 정답 출력.
+	cout << opt[n][k];
 }
 
 int main() {
@@ -52,11 +49,11 @@ int main() {
 	cin.tie(nullptr); cout.tie(nullptr);
 
 	cin >> n >> k;
-	
+
 	for (int i = 1; i <= n; i++) {
 		cin >> weight[i] >> value[i];
 	}
-	dp(n,k);
+	dp(n, k);
 
 
 
